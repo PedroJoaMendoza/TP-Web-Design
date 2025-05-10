07d69b2ab561a9539b3e81d70567f2af6def65dd
pedro Mendoza <pedrojoaquinmendoza@gmail.com>
2025-05-10 02:14:47 -0300

    Only ADD left

--- script.js
+++ script.js
@@ -1,105 +1,136 @@
 const API_URL = "https://jsonplaceholder.typicode.com";
+const formulario = document.getElementById("addStudentForm")
 
-async function CargarAlumnos() {
-    const tabla = document.getElementById("tabla");
-    if(!tabla) return;
-    try{
-        const response =await fetch(`${API_URL}/posts`);
-        const alumnos =await response.json();
-        tabla.innerHTML="";
-        
-        alumnos.forEach(est => {
-            const row = tabla.insertRow();
-            const idCell = row.insertCell(0);
-            const titleCell = row.insertCell(1);
-            const bodyCell = row.insertCell(2);
-            const actionsCell = row.insertCell(3);
-            
-            row.setAttribute('id',est.id);
-            idCell.innerHTML = est.id;
-            titleCell.innerHTML = est.title;
-            bodyCell.innerHTML = est.body;
-            actionsCell.innerHTML = `<button onclick="editarEstudiante(${est.id})">Editar</button> <button onclick="eliminarPost(${est.id})">Eliminar</button>`;
-            tabla.appendChild(row);
-        });
-        window.addEventListener("DOMContentLoaded",()=>{
-        
-        const data = localStorage.getItem("newPOST");
-        if (data) {
-            const response = JSON.parse(data)
-            
-            const tabla = document.getElementById('tabla');
-            const row = tabla.insertRow();
-            const idCell = row.insertCell(0);
-            const titleCell = row.insertCell(1);
-            const bodyCell = row.insertCell(2);
-            const actionsCell = row.insertCell(3);
-            
-            row.setAttribute('id',response.id);
-            idCell.innerHTML = response.id;
-            titleCell.innerHTML = response.title;
-            bodyCell.innerHTML = response.body;
-            actionsCell.innerHTML = `<button onclick="editarEstudiante(${response.id})">Editar</button> <button onclick="eliminarPost(${response.id})">Eliminar</button>`;
-            tabla.appendChild(row);
-            
-            localStorage.removeItem("newPOST");
-        }
-    })
-
-    } catch(error){
-        console.error("Error al cargar estudiante",error)
+function ResetAll() {
+    if (confirm("Seguro que quiere reiniciar los datos?")) {
+        localStorage.clear();
+        location.reload();
     }
 }
 
-const formulario = document.getElementById("addStudentForm")
-if(formulario){
-    formulario.addEventListener("submit",async(e)=>{
+
+if (formulario) {
+    formulario.addEventListener("submit", async (e) => {
         e.preventDefault();
-        const newrow = {
+        const newPOST = {
+            id: Date.now(),
             title: document.getElementById("title").value,
             body: document.getElementById("body").value,
         };
-        try{
-            let response = await fetch(`${API_URL}/posts`,{
-                method: "POST",
-                headers:{"Content-Type":"aplication/json"},
-                body:JSON.stringify(newrow)
-            });
-            response = await response.json();
-            localStorage.setItem("newPOST",JSON.stringify(response));
+        try {
+            const localPosts = JSON.parse(localStorage.getItem("localPosts")) || [];
+            localPosts.unshift(newPOST);
+            localStorage.setItem("localPosts", JSON.stringify(localPosts));
+
             alert("Linea agregado correctamente.");
             window.location.href = "index.html";
-            localStorage.setItem();
-            
-        }catch (error){
-            console.error("Error al agregar alumno",error);
+
+        } catch (error) {
+            console.error("Error al agregar alumno", error);
         }
     });
 }
 
-async function editarPost(id) {
+window.onload = () => {
+    const tabla = document.getElementById("tabla");
+    if (!tabla) return;
 
-    
+    async function InizialiceData() {
+        if (!localStorage.getItem("apiPosts")) {
+            try {
+                const response = await fetch(`${API_URL}/posts`)
+                const data = await response.json();
+                localStorage.setItem("apiPosts", JSON.stringify(data));
+            } catch (error) {
+                console.error("Error al traer la API", error);
+                return;
+            }
+        }
+        ShowTable();
+    }
+
+    function ShowTable() {
+        tabla.innerHTML = "";
+
+        const localPost = JSON.parse(localStorage.getItem("localPost")) || [];
+
+        const apiPosts = JSON.parse(localStorage.getItem("apiPosts")) || [];
+
+        const allpost = [...localPost, ...apiPosts];
+
+        allpost.forEach(post => {
+
+            const row = tabla.insertRow();
+            row.setAttribute("id", post.id);
+
+            row.insertCell(0).innerHTML = post.id;
+            row.insertCell(1).innerHTML = post.title;
+            row.insertCell(2).innerHTML = post.body;
+
+            const acction = row.insertCell(3);
+            acction.innerHTML = `<button onclick="editRow(${post.id})">Editar</button> <button onclick="eliminarPost(${post.id})">Eliminar</button>`;
+        });
+    }
+    InizialiceData();
 }
 
-async function eliminarPost(id){
-    if(!confirm("Seguro que quiere eliminar la linea?"))return;
+function editRow(id) {
+    const row = document.getElementById(id);
+    if (!row) return;
 
-    try{
-        await fetch(`${API_URL}/posts/${id}`,{
+    const newTitle = prompt("New Title", row.cells[1].innerHTML);
+    const newBody = prompt("New Body", row.cells[2].innerHTML);
+
+    if (newTitle !== null) row.cells[1].innerHTML = newTitle;
+    if (newBody !== null) row.cells[2].innerHTML = newBody;
+
+    updatedPosts(id, newTitle, newBody);
+}
+
+async function eliminarPost(id) {
+    if (!confirm("Seguro que quiere eliminar la linea?")) return;
+
+    try {
+        await fetch(`${API_URL}/posts/${id}`, {
             method: "DELETE"
         });
-        const table = document.getElementById('tabla');
-        const deletedRow = document.getElementById(id);
-        table.removeChild(deletedRow);
+        const row = document.getElementById(id);
+        if (row) row.remove();
+
+        let localPosts = JSON.parse(localStorage.getItem("localPosts"))||[];
+        localPosts = localPosts.filter(p=> p.id !== id);
+        localStorage.setItem("localPosts",JSON.stringify(localPosts));
+
+        let apiPosts = JSON.parse(localStorage.getItem("apiPosts"))||[];
+        apiPosts = apiPosts.filter(p => p.id != id);
+        localStorage.setItem("apiPosts", JSON.stringify(apiPosts));
+
         alert("Linea eliminado.");
-    }catch (error){
+    } catch (error) {
         console.error("Error al eliminar la linea");
     }
 }
+async function updatedPosts(id, newTitle, newBody) {
 
-var counter = 0;
+    const localPost = JSON.parse(localStorage.getItem("localPosts")) || [];
+    const idxLocal = localPost.findIndex(p => p.id === id);
 
-window.onload = () =>{
-        CargarAlumnos();
+    if (idxLocal !== -1) {
+        localPost[idxLocal].title = newTitle;
+        localPost[idxLocal].body = newBody;
+        localStorage.setItem("localPosts", JSON.stringify(localPost));
+        return;
+    }
+
+    let apiPosts = JSON.parse(localStorage.getItem("apiPosts")) || [];
+    const idxApi = apiPosts.findIndex(p => p.id === id);
+    if (idxApi != -1) {
+        await fetch(`${API_URL}/posts/${id}`, {
+            method: "PUT"
+        });
+
+        apiPosts[idxApi].title = newTitle;
+        apiPosts[idxApi].body = newBody;
+        localStorage.setItem("apiPosts", JSON.stringify(apiPosts));
+    }
 }
